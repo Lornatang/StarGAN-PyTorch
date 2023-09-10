141f694703224128507a9dffb4b342a4b0b95943
Lornatang <liuchangyu1111@gmail.com>
2023-09-10 19:55:51 +0800

    Update

---
+++ scripts/download_dataset.sh
@@ -0,0 +1,15 @@
+FILE=$1
+
+if [ "$FILE" == "celeba" ]; then
+
+    # CelebA images and attribute labels
+    URL="https://www.dropbox.com/s/d1kjpkqklf0uw77/celeba.zip?dl=0"
+    ZIP_FILE=./data/celeba.zip
+    mkdir -p ./data/
+    wget -N "$URL" -O $ZIP_FILE
+    unzip $ZIP_FILE -d ./data/
+    rm $ZIP_FILE
+else
+  echo "Available arguments are celeba."
+  exit 1
+fi
