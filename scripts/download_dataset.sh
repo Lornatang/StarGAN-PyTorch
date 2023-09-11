a5a92ab134d2a0e7a438141d52f47b7acb66a47e
Lornatang <liuchangyu1111@gmail.com>
2023-09-11 18:21:52 +0800

    Upgrade

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,15 +1,14 @@
 FILE=$1
 
-if [ "$FILE" == "celeba" ]; then
-
-    # CelebA images and attribute labels
-    URL="https://www.dropbox.com/s/d1kjpkqklf0uw77/celeba.zip?dl=0"
-    ZIP_FILE=./data/celeba.zip
-    mkdir -p ./data/
-    wget -N "$URL" -O $ZIP_FILE
-    unzip $ZIP_FILE -d ./data/
-    rm $ZIP_FILE
+if [ "$FILE" == "StarGAN_CelebA" ]; then
+  # Download the imagenet dataset and move validation images to labeled subfolders
+  URL="https://huggingface.co/datasets/goodfellowliu/StarGAN_CelebA/resolve/main/celeba.zip"
+  ZIP_FILE=./data/celeba.zip
+  mkdir -p ./data
+  wget -N $URL -O $ZIP_FILE
+  unzip $ZIP_FILE -d ./data
+  rm $ZIP_FILE
 else
-  echo "Available arguments are celeba."
+  echo "Available arguments are StarGAN_CelebA."
   exit 1
 fi
